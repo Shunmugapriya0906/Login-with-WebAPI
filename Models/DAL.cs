8703d9284a24fd642dc60bcd473e537e099f1383
Shunmuga Priya B <132599852+Shunmugapriya0906@users.noreply.github.com>
2024-08-07 19:04:39 +0530

    Add files via upload

---
+++ Models/DAL.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+using System.Data;
+
+namespace LoginWebApi.Models
+{
+    public class DAL
+    {
+        SqlConnection con=new SqlConnection(ConfigurationManager.ConnectionStrings["webapicon"].ConnectionString);
+        SqlDataAdapter da = null;
+        DataTable dt = null;
+        public string GetLogin(Login login)
+        {
+            da = new SqlDataAdapter("select * from users where Username='" + login.Username +"' and password= '" + login.password + " ' ", con);
+            dt=new DataTable();
+            da.Fill(dt);
+            if(dt.Rows.Count > 0 )
+            {
+                return "Logged In";
+            }
+            else
+            {
+                return "Login Failed";
+            }
+
+        
+        }
+    }
+}
---
+++ Scripts/PageScripts/index.js
@@ -0,0 +1,19 @@
+﻿var GetLogin = function () {
+    var Username = $('#txtUsername').val();
+    var Password = $('#txtpassword').val();
+    var url = "/api/values/GetLogin";
+    var data = JSON.stringify({ "Username": Username, "password": password });
+    $.ajax({
+        type: "Post",
+        data: loginData,
+        url: loginUrl,
+        contentType: "application/json;charset=utf-8",
+        dataType: "json",
+        success: function (result) {
+            alert(result)
+        },
+        error: function (result) {
+            alert(result)
+        }
+    });
+};
